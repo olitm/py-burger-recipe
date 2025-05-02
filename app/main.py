06cabae4210c6b89099b701ef1105675d5f514c9
Ol <holmate@dontsp.am>
2025-05-02 19:14:11 +0300

    Fixed incorrect types in annotations

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Validator(ABC):
-    def __set_name__(self, owner: classmethod, name: str) -> None:
+    def __set_name__(self, owner: type, name: str) -> None:
         self.protected_name = "_" + name
 
-    def __get__(self, instance: object, owner: classmethod) -> str | int:
+    def __get__(self, instance: object, owner: type) -> str | int:
         return getattr(instance, self.protected_name)
 
     def __set__(self, instance: object, value: str | int) -> None:
